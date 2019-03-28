7c5300862cafe469b4fe9a04494e41ae328e9c7e
Craftingexpert1 <34014055+Craftingexpert1@users.noreply.github.com>
2019-03-28 10:07:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+document.querySelector('#play-btn').addEventListener('click',()=>{
+  document.querySelector('#home').style.display="none";
+  document.querySelector('#game').style.display="block"
+});
