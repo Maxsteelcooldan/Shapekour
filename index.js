cfdf2059c62681c6ec0e56f2ee3980823ac7f0a5
Craftingexpert1 <34014055+Craftingexpert1@users.noreply.github.com>
2019-03-28 15:00:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ document.querySelector('#play-btn').addEventListener('click',()=>{
   document.querySelector('#home').style.display="none";
   document.querySelector('#game').style.display="block"
 });
+cnvs = document.querySelector('canvas');
+yat = cnvs.getContext('2d');
