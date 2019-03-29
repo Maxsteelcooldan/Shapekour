379b0540e44cf1c99e2f44079043a746dbb9eefe
Craftingexpert1 <34014055+Craftingexpert1@users.noreply.github.com>
2019-03-29 11:41:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ document.querySelector('#play-btn').addEventListener('click',()=>{
 });
 cnvs = document.querySelector('canvas');
 yat = cnvs.getContext('2d');
+function rect(x, y, width, height, color) {
+  this.width = width || 50;
+  this.height = height || 50;
+  this.x = x || 0;
+  this.y = y || 0; 
+  this.color = color || 'black';
+  yat.fillStyle = color;
+  yat.fillRect(this.x, this.y, this.width, this.height);
+}
