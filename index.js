58d3e604fdbe4289149ef072a4187570068810de
LightspeedCoder <48801499+LightspeedCoder@users.noreply.github.com>
2019-04-01 17:54:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ document.querySelector('#play-btn').addEventListener('click',()=>{
   document.querySelector('#home').style.display="none";
   document.querySelector('#game').style.display="block";
   game = setInterval(levels[0], 30);
-  alert();
 });
+
 cnvs = document.querySelector('canvas');
 yat = cnvs.getContext('2d');
 function rect(x, y, width, height, color) {
